eda96ba24085cfb67e6a9ebcf23727116c2fec4e
Travis Bender <65250492+travbend@users.noreply.github.com>
2024-12-19 19:43:13 -0500

    Progress commit

--- scripts/content.js
+++ scripts/content.js
@@ -4,6 +4,11 @@ s.src = chrome.runtime.getURL('scripts/handle-click.js');
 s.onload = function() { this.remove(); };
 (document.head || document.documentElement).appendChild(s);
 
+var s2 = document.createElement('script');
+s2.src = "https://cdn.jsdelivr.net/npm/@ffmpeg/ffmpeg@0.12.10/dist/umd/ffmpeg.min.js";
+s2.onload = function() { this.remove(); };
+(document.head || document.documentElement).appendChild(s2);
+
 let captionsButton = document.getElementById("captionsButton");
 
 if (captionsButton) {
