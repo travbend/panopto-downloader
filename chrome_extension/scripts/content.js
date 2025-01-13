78d3b569c7549d761baff34610baf83611c813f7
Travis Bender <65250492+travbend@users.noreply.github.com>
2025-01-12 21:56:05 -0500

    Downloading file in chrome extension

--- chrome_extension/scripts/content.js
+++ chrome_extension/scripts/content.js
@@ -7,9 +7,24 @@ let captionsButton = document.getElementById("captionsButton");
 
 if (captionsButton) {
     let downloadButton = document.createElement("div");
-    // downloadButton.style.width = "40 px"
+    downloadButton.style.width = "60px"
     downloadButton.classList.add("transport-button");
     downloadButton.textContent = "Download";
-    downloadButton.setAttribute("onclick", "window.GetVideoUrl()");
+    downloadButton.setAttribute("onclick", "window.downloadVideo()");
     captionsButton.insertAdjacentElement("afterend", downloadButton);
 }
+
+(async () => {
+    try {
+        const response = await fetch(chrome.runtime.getURL('scripts/popup.html'));
+        const html = await response.text();
+        
+        const container = document.createElement('div');
+        container.id = "downloaderContainer";
+        container.style.display = 'none';
+        container.innerHTML = html;
+        document.body.appendChild(container);
+    } catch (error) {
+        console.error('Error inserting HTML:', error);
+    }
+})();
--- chrome_extension/scripts/handle-click.js
+++ chrome_extension/scripts/handle-click.js
@@ -1,5 +1,17 @@
 
-async function getVideoUrl() {
+const apiUrl = "https://pd.travbend.com/panopto-downloader/api/v1/convert-to-mp4"
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function downloadVideo() {
+    let downloadText = document.getElementById("downloaderPopupText");
+    downloadText.textContent = "Decoding file...";
+
+    let container = document.getElementById("downloaderContainer");
+    container.style.display = 'flex';
+
     try {
         const queryParams = new URLSearchParams(window.location.search);
         let deliveryId = queryParams.get('id') ?? Panopto.viewer.data.playlist.initialDeliveryId;
@@ -29,13 +41,86 @@ async function getVideoUrl() {
         let fileName = responseBody.Delivery.Streams[0].Name;
         let videoUrl = responseBody.Delivery.Streams[0].StreamHttpUrl;
 
-        console.log(videoUrl)
+        const initiateUrl = apiUrl + '/initiate';
+        const data = { 
+            'video_url': videoUrl,
+            'file_name': fileName
+        };
 
-        alert(videoUrl);
+        let initiateResponse = await fetch(initiateUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        if (!initiateResponse.ok || !initiateResponse.body) {
+            throw new Error('Failed to convert the file');
+        }
+
+        let initiateRBody = await initiateResponse.json();
+
+        const statusUrl = apiUrl + '/status/' + initiateRBody.task_id;
+
+        let status = null;
+
+        while (true) {
+            let statusResponse = await fetch(statusUrl);
+
+            if (!statusResponse.ok || !statusResponse.body) {
+                throw new Error('Failed to convert the file');
+            }
+
+            let statusRBody = await statusResponse.json();
+            status = statusRBody.status;
+
+            if (statusRBody.status != "PENDING")
+                break;
+
+            await sleep(1000);
+        }
+
+        if (status != "COMPLETED")
+            throw new Error("Failed to convert file");
+
+        downloadText.textContent = "Downloading file...";
+
+        const resultUrl = apiUrl + '/result/' + initiateRBody.task_id;
+        const resultResponse = await fetch(resultUrl);
+        
+        if (!resultResponse.ok || !resultResponse.body) {
+            throw new Error('Failed to download the file');
+        }
+
+        const reader = resultResponse.body.getReader();
+        let receivedLength = 0;
+        const chunks = [];
+
+        while (true) {
+            const { done, value } = await reader.read();
+            if (done) {
+                break;
+            }
+            chunks.push(value);
+            receivedLength += value.length;
+        }
+
+        let blob = new Blob(chunks);
+        const downloadUrl = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = downloadUrl;
+        a.download = fileName;
+        document.body.appendChild(a);
+        a.click();
+        a.remove();
+        URL.revokeObjectURL(downloadUrl);
 
     } catch (e) {
-        alert("An error occurred while downloading the video.");
+        alert("An error occurred while downloading the video. Please try again later.");
+    } finally {
+        container.style.display = 'none';
     }
 }
 
-window.GetVideoUrl = getVideoUrl;
+window.downloadVideo = downloadVideo;
